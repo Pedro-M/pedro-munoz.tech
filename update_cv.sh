5214dbf5bfdc7548e89796d46893a343e4e1dd80
Pedro <p.munozb2103@gmail.com>
2019-09-19 18:24:18 +0200

    testing GL-GH integration

--- update_cv.sh
+++ update_cv.sh
@@ -2,4 +2,5 @@ cp ../../../cvs/pedro/pedro_munoz_cv.pdf content/downloads/pedro-munoz-cv.pdf
 git add content/downloads/pedro-munoz-cv.pdf
 git commit -m 'Updated cv'
 git push
-ssh gurus-server 'cd projects/thegurus/pedro-munoz.tech/; git pull; source ~/.venvs/pedro-munoz_env/bin/activate; make publish'
+ssh gurus-prod 'cd projects/thegurus/pedro-munoz.tech/; git pull; source ~/.venvs/pedro-munoz_env/bin/activate; make publish'
+
