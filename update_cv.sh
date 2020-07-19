63aea7e3dc075d334207a8ef339f79a14aac0dfb
pedro-munoz <pedro.munoz@whiteboxml.com>
2020-07-19 21:30:41 +0200

    removed all but README

--- update_cv.sh
+++
@@ -1,6 +0,0 @@
-cp ../../../shared/cvs/pedro/pedro_munoz_cv.pdf content/downloads/pedro-munoz-cv.pdf
-git add content/downloads/pedro-munoz-cv.pdf
-git commit -m 'Updated cv'
-git push
-ssh gurus-prod 'cd projects/thegurus/pedro-munoz.tech/; git pull; source ~/.venvs/pedro-munoz_env/bin/activate; make publish'
-
